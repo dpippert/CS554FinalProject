c0e7f995d7bfb26cfe8e41638db2f205732c2558
Dale Pippert <dpippert@wsfsbank.com>
2021-05-19 18:21:44 -0400

    working on home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,18 +10,22 @@ function Home(){
           <h1></h1> <h2></h2> <h3></h3> <h4></h4>
           <br/>
           <br/>
-        <div className="card">
+        <div className="card text-center">
         
         <div className="card-body">
-          <h5 className="card-title"> How to Play?</h5>
-          <p className="card-text">
-<br />1.	Game play begins by directing players to the game screen, where a game board appears.
-<br />2.	The game board appears as a matrix with a top row of 5 hidden topic names. Each topic consists of a column of 5 hidden questions (for a total of 25 questions for the game).
-<br />3.	Game board is on the upper portion of a player's screen.
-<br />4.	Lower portion of the game player screen consists of various status, monitoring, and control resources.
-<br />5.	All game play occurs on game screen.
-
-</p>
+          <h5 className="card-title">Our Demo Game</h5>
+          <br/>
+          <p className="card-text">Using our question and answer repository in conjunction with our real-time
+          gaming engine, we offer to you a free game we call</p>
+          <div>
+          <img className="card-text" src="./topictempest_small.png" title="Topic Tempest" alt="Topic Tempest"/>
+          </div>
+          <br/>
+          <p className="card-text">Currently configured for two players, Topic Tempest throws up a Jeopardy-like
+          format, a setting that should be familiar to most. It showcases our backend questions repository
+          and cloud-based real-time database, as key enabling technologies to get you thinking about how you can bring
+          a fun and dynamic learning experience to your students, clients, members, and more.
+          </p>
           <a href="/play" className="btn btn-primary">Ready To  Play!</a>
         </div>
       </div> 
@@ -30,7 +34,7 @@ function Home(){
         <div className="card">
        
         <div class="card-body">
-          <h5 class="card-title">Adding question and answer</h5>
+          <h5 class="card-title">Our Question and Answer Portal</h5>
           <br/>
           <p class="card-text">To add question and answers to the database one has to sign up.<br/>
           There are options on how one can Sign In.
@@ -41,7 +45,7 @@ function Home(){
           One can add question and answers to the database with the topics.<br/>
           The question can have multiple answers<br/>
           </p>
-          <a href="/admin" className="btn btn-primary">Add question !</a>
+          <a href="/admin" className="btn btn-primary">Let's try it out!</a>
         </div>
       </div> 
       <br/>
@@ -50,11 +54,17 @@ function Home(){
       
         <div className="card-body">
           <h5 className="card-title">Team  Tempest</h5>
-          <p className="card-text"><br/><a  href="mailto:kyle.bernardes@gmail.com">Kyle Bernardes</a> CWID 10412644
+          <br/>
+          <p className="card-text">Contact us to learn more about how you can leverage our question/answer repository and real-time gaming
+          engine components for your corporate training or professional learning gamification ideas!
+          </p>
+          <p className="card-text"><a  href="mailto:kyle.bernardes@gmail.com">Kyle Bernardes</a> CWID 10412644
           <br/><a href="mailto:mchandan@stevens.edu">Mahima Chandan </a>CWID 10467889 
           <br/><a href="mailto:dale.pippert@gmail.com">Dale Pippert</a> CWID 10469880</p>
           <a href="https://github.com/dpippert/CS554FinalProject" className="btn btn-success">GitHub URL</a>
+          <br/>
           <a href="https://github.com/dpippert/CS554FinalProject/blob/master/doc/Arch.png" className="btn btn-primary">Flow of the project</a>
+          <br/>
         </div>
       </div> 
       </div>
